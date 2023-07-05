bf0d3fd45e7aaa0b89a040939c2cfab7433eda08
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-07-05 20:16:10 +0300

    1234

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import SignUpForm from './Feedback/feedback'
+
+
 export const App = () => {
   return (
     <div
@@ -7,9 +10,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
+      <SignUpForm title="Upload stats" />
       React homework template
     </div>
   );
